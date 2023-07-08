7862bf716160954f8f789fb3c19fecfd48641d2d
Alex Dulaney <18Dulaneyaw@gmail.com>
2023-07-07 23:23:59 -0400

    complete lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,31 @@
-#!/usr/bin/env python3
-
 def happy_new_year():
-    # code goes here!
-    pass
+    counter = 10
+    while counter > 0:
+        print(counter)
+        counter -= 1
+    print("Happy New Year!")
 
-def square_integers(int_list):
-    # code goes here!
-    pass
+def fizzbuzz_printer():
+    for num in range(1, 101):
+        print(fizzbuzz(num))
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+
+
+def reverse_string(string):
+    reversed_str = ""
+    for i in range(len(string) - 1, -1, -1):
+        reversed_str += string[i]
+    return reversed_str
+
+def square_integers(int_list):
+    return [num ** 2 for num in int_list]
